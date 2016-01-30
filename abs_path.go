1fec5d6768e4b11dc83508339cf505d5532a6a78
Alex Nichol <unixpickle@gmail.com>
2016-01-30 10:16:46 -0500

    created tool for getting absolute path for relative one

---
+++ abs_path.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/unixpickle/svgdemos/svg"
+)
+
+func main() {
+	fmt.Println("Enter path data, then deliver an EOF:")
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to read data:", err)
+		os.Exit(1)
+	}
+	path, err := svg.ParsePath(string(data))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to parse:", err)
+		os.Exit(1)
+	}
+	fmt.Println("Absolute path:")
+	fmt.Println(path.Absolute().String())
+}
